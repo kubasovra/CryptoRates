8c7145b5193578671aa7163de151e333f9b6ee35
Roman Kubasov <kubasovra@gmail.com>
2020-02-10 15:27:53 +0300

    Troubles with migrations

---
+++ CryptoRates/Models/CryptoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoRates.Models
+{
+    public class CryptoContext : IdentityDbContext
+    {
+        public CryptoContext(DbContextOptions<CryptoContext> options) : base(options)
+        {
+            this.Database.Migrate();
+        }
+    }
+}
